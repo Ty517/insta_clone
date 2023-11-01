6fcc0114d0655a90de198bf4a6c2ce747a24f7ee
Ty517 <tunganeville@gmail.com>
2023-11-01 09:22:41 +0200

    addeed lines to gitignore and routes and changed the variable for controller and removed package lock.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const route1 = require('../controller');
+const controller = require('../controller');
 const route = express.Router();
-route.get('/', route1);
-module.exports = route1;
-
+route.get('/', controller);
+module.exports = route;
