878bb8767aaab2a353f6d797435ae09d8fb4f09c
Ty517 <tunganeville@gmail.com>
2023-11-13 16:01:40 +0200

    Implemented forgot password

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const authController = require('../controller/authController');
 const validateSignup = require('../middleware/validateSignup');
 const validatelogin = require('../middleware/validateSignup');
+const forgot = require('../middleware/validateSignup');
 
 const route = express.Router();
-route.post('/signup', validateSignup, authController.signup);
+route.post('/signup', validateSignup.validateSignup, authController.signup);
 route.get('/confirm/:token', authController.confirmEmail);
-route.post('/login', validatelogin, authController.login);
+route.post('/login', validatelogin.validatelogin, authController.login);
+route.post('/forgot', forgot.forgot, authController.forgotpass);
 
 module.exports = route;
